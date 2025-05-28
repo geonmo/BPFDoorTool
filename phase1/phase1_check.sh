6d39e30209ca2e02760c91bb27df1bda6808700e
root <root@cms-t2-wn1010.sdfarm.kr>
2025-05-28 11:10:11 +0900

    update phase1_check.

---
+++ phase1/phase1_check.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ss -0pb | grep -EB1 --colour "$((0x7255))|$((0x5293))|$((0x39393939))"
+DIRS="/dev /var /bin /usr /opt /local"
+for path in $DIRS; do
+	echo $path
+	find $path -type f -exec sh -c 'hexdump -ve "1/1 \"%.2x\"" "$1" | grep -q "c6459049c6459135c645922ac6459341c6459459c6459562" && echo "$1"' _ {} \;
+done
+netstat -lpn | grep -E ':42[3-9][0-9]{2}|43[0-3][0-9]{2}'
