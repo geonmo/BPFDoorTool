199753a4f9da61bce192a59df25832bf37c36b04
Geonmo Ryu <geonmo@kisti.re.kr>
2025-05-28 11:50:12 +0900

    update ignore paths.

--- phase1/phase1_check.sh
+++ phase1/phase1_check.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 ss -0pb | grep -EB1 --colour "$((0x7255))|$((0x5293))|$((0x39393939))"
-DIRS="/dev /var /bin /usr /opt /local"
+DIRS="/dev /var /bin /usr /opt /local /share"
+netstat -lpn | grep -E ':42[3-9][0-9]{2}|43[0-3][0-9]{2}'
 for path in $DIRS; do
 	echo $path
-	find $path -type f -exec sh -c 'hexdump -ve "1/1 \"%.2x\"" "$1" | grep -q "c6459049c6459135c645922ac6459341c6459459c6459562" && echo "$1"' _ {} \;
+	find $path -type f ! \( -type d -path '/var/lib/cvmfs' -o -path '/share/geonmo2' -o -path '/share/kong91' -prune \)  -exec sh -c 'hexdump -ve "1/1 \"%.2x\"" "$1" | grep -q "c6459049c6459135c645922ac6459341c6459459c6459562" && echo "$1"' _ {} \;
 done
-netstat -lpn | grep -E ':42[3-9][0-9]{2}|43[0-3][0-9]{2}'
